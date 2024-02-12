dc8df8b53efee98cc8c73278e119e2b0470fd226
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2024-02-12 17:53:12 +0100

    fix

--- src/FSRU_IWSPE.jl
+++ src/FSRU_IWSPE.jl
@@ -3,5 +3,7 @@ using Reexport
 @reexport using JuMP, HiGHS, DataStructures, Graphs, MetaGraphs
 include("graph_construction.jl")
 include("graph_plotting.jl")
+include("model_greenfield.jl")
+include("model_brownfield.jl")
 include("api.jl")
 end
--- src/api.jl
+++ src/api.jl
@@ -1,12 +1,12 @@
 export generate_map_greenfield, generate_map_brownfield
 
 function generate_map_greenfield()
-    include("src/model_greenfield.jl")
+    g, consumers_dict, domestic_dict, port_dict, import_dict, export_dict, ports_coordinates, penalized_arcs = solve_greenfield()
     FSRU_IWSPE.GLMakie.activate!(inline=false)
     display(map_network(g, consumers_dict, domestic_dict, port_dict, import_dict, export_dict, ports_coordinates, highlight_arcs = penalized_arcs))
 end
 function generate_map_brownfield()
-    include("src/model_brownfield.jl")
+    g, consumers_dict, domestic_dict, port_dict, import_dict, export_dict, ports_coordinates, penalized_arcs = solve_brownfield()
     FSRU_IWSPE.GLMakie.activate!(inline=false)
     display(map_network(g, consumers_dict, domestic_dict, port_dict, import_dict, export_dict, ports_coordinates, highlight_arcs = penalized_arcs))
 end
